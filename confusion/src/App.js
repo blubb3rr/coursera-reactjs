e7002adc725f37e4e9fc1ab7e377919b91ad8518
blubb3rr <dkansal_be17@thapar.edu>
2020-06-08 19:26:44 +0530

    vs

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -3,10 +3,6 @@ import Main from './components/MainComponent'
 import './App.css';
 import {BrowserRouter} from 'react-router-dom';
 class App extends Component{
-  constructor(props){
-  super(props);
-}
-
 render(){
   return (
     <BrowserRouter>
